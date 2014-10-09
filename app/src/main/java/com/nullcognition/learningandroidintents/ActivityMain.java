9b34bba9796d67479245bcb53d942fd0ace3bba0
ersin <ersin_the_ertan@hotmail.com>
2014-10-08 21:59:22 -0400

    Chapter 2 Complete - send and recieve data from an activity with stratActivityForResult() and onActivityResult()

--- app/src/main/java/com/nullcognition/learningandroidintents/ActivityMain.java
+++ app/src/main/java/com/nullcognition/learningandroidintents/ActivityMain.java
@@ -12,10 +12,30 @@ public class ActivityMain extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+	  android.widget.Button button = (android.widget.Button)findViewById(com.nullcognition.learningandroidintents.R.id.button);
+	  button.setOnClickListener(new android.view.View.OnClickListener() {
 
+		@Override
+		public void onClick(android.view.View inView){
+		  android.content.Intent intentToAcivityFrom = new android.content.Intent(getApplicationContext(), ActivityFromIntent.class);
+		  intentToAcivityFrom.putExtra("intentToAcivityFrom", "value");
+		  startActivityForResult(intentToAcivityFrom, 1);
+		}
+	  });
+	}
 
-    @Override
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, android.content.Intent data){
+	super.onActivityResult(requestCode, resultCode, data);
+	if(requestCode == 1 && resultCode == RESULT_OK){
+	  android.widget.Toast.makeText(this, "ok", android.widget.Toast.LENGTH_SHORT).show();
+	}
+	if(resultCode == RESULT_CANCELED){
+	  android.widget.Toast.makeText(this, "not ok", android.widget.Toast.LENGTH_SHORT).show();
+	}
+  }
+
+  @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
