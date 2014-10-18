0215a61c40e44866355977df6be82ac570ff1f3f
ersin <ersin_the_ertan@hotmail.com>
2014-10-17 22:18:45 -0400

    Chapter 4 - Intents to check if tts is enabled and to download a lang pack if not
    updated api and gradle to build tools 21 and .13

--- app/src/main/java/com/nullcognition/learningandroidintents/ActivityMain.java
+++ app/src/main/java/com/nullcognition/learningandroidintents/ActivityMain.java
@@ -10,6 +10,7 @@ public class ActivityMain extends Activity {
 
   public final int blueToothDiscoverableCode = 1001;
   int blueToothRequestCode = 101;
+  int speechRequestCode = 123;
   com.nullcognition.learningandroidintents.BluetoothStateReceiver btsr;
 
   @Override
@@ -61,9 +62,34 @@ public class ActivityMain extends Activity {
 	  }
 	});
 
+	android.widget.Button buttonVoiceRecognition = (android.widget.Button)findViewById(
+	  com.nullcognition.learningandroidintents.R.id.buttonVoiceRecognition);
+	buttonVoiceRecognition.setOnClickListener(new android.view.View.OnClickListener() {
+
+	  @Override
+	  public void onClick(android.view.View inView){
+		android.content.Intent i = new android.content.Intent(android.speech.RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		i.putExtra(android.speech.RecognizerIntent.EXTRA_LANGUAGE_MODEL, android.speech.RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+		i.putExtra(android.speech.RecognizerIntent.EXTRA_PROMPT, "Speaking");
+		i.putExtra(android.speech.RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+		i.putExtra(android.speech.RecognizerIntent.EXTRA_LANGUAGE, java.util.Locale.ENGLISH);
+		startActivityForResult(i, speechRequestCode); // active internet connection is required
+	  }
+	});
+
+	checkForTTS();
+
 	initBluetooth();
 
 	blueToothDiscoverable();
+  }
+
+  final int valttsdata = 14;
+
+  private void checkForTTS(){
+	// check for text to speech
+	android.content.Intent i = new android.content.Intent(android.speech.tts.TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+	startActivityForResult(i, valttsdata);
   }
 
   @Override
@@ -101,6 +127,33 @@ public class ActivityMain extends Activity {
   @Override
   protected void onActivityResult(int requestCode, int resultCode, android.content.Intent data){
 	super.onActivityResult(requestCode, resultCode, data);
+
+	// look into the new way of dealing with this tts from api 21
+//
+//	if(requestCode == valttsdata && resultCode == android.speech.tts.TextToSpeech.Engine.CHECK_VOICE_DATA_PASS){
+//	  final android.speech.tts.TextToSpeech tts = new android.speech.tts.TextToSpeech(this, new android.speech.tts.TextToSpeech.OnInitListener() {
+//
+//		public void onInit(int status){
+//		  if(status == android.speech.tts.TextToSpeech.SUCCESS){
+//			tts.setLanguage(java.util.Locale.US);
+//			tts.setSpeechRate(1.1f);
+//			tts.speak("Hello, I am writing book for Packt", android.speech.tts.TextToSpeech.QUEUE_ADD, null,null);
+//		  }
+//		}
+//	  });
+//	}
+//	else{
+//	  android.widget.Toast.makeText(this, "no tts available", android.widget.Toast.LENGTH_SHORT).show();
+//	  android.content.Intent installLanguage = new android.content.Intent(android.speech.tts.TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+//	  startActivity(installLanguage);
+//	}
+
+	if(requestCode == speechRequestCode && resultCode == android.app.Activity.RESULT_OK){
+	  java.util.ArrayList<String> results;
+	  results = data.getStringArrayListExtra(android.speech.RecognizerIntent.EXTRA_RESULTS);
+	  android.widget.EditText et = (android.widget.EditText)findViewById(com.nullcognition.learningandroidintents.R.id.editText);
+	  et.setText(results.toString());
+	}
 	if(requestCode == 1 && resultCode == RESULT_OK){
 	  android.widget.Toast.makeText(this, "ok", android.widget.Toast.LENGTH_SHORT).show();
 	  //new android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse("http://www.google.ca/"));
